c47cdd2b06a846ce4a69edbaf789cf34fac2a655
Ahmed Mostafa <a.mostafa.0x61@gmail.com>
2020-05-25 02:53:25 +0200

    Optimizing

--- Cimaclub Movies.py
+++ Cimaclub Movies.py
@@ -1,32 +1,35 @@
 from bs4 import BeautifulSoup
-import requests
-import re
+from requests import get
+from re import compile
 
-in_file = r"C:\Users\Ahmed Mostafa\Desktop\CimaClub\movies.txt"
-out_file = r"C:\Users\Ahmed Mostafa\Desktop\CimaClub\MLinks.txt"
+from os import path
+'''
+This Script takes the text file with movie links and returns direct download links
+'''
+
+# Replace {Path} with Movies file Path
+in_file = r"Path"
+
+dct = path.dirname(path.abspath(__file__))
+
+
+out_file = f"{dct}" + "\MLinks.txt"
+
+
 links = []
 with open(in_file)as file:
     for line in file:
         if len(line) > 1:
-            req = requests.get(line)
+            req = get(line)
             soup = BeautifulSoup(req.text, "lxml")
-            page = soup.find('a', attrs={'href': re.compile(".*download=.*govid.*")}).get('href')
+            print("Done")
+            page = soup.find('a', attrs={'href': compile(".*download=.*govid.*")}).get('href')
 
-            req = requests.get(page).text
-            soup = BeautifulSoup(req, "lxml")
+            req = get(page)
+            soup = BeautifulSoup(req.text, "lxml")
             # Quality Change -->(5)<--   [6 for 720]  [5 for 480]  [4 for 360]
-            page = soup.find('a', attrs={'onclick': re.compile(".*( 5.* ).*")}).get('href')
+            page = soup.find('a', attrs={'onclick': compile(".*( 5.* ).*")}).get('href')
             links.append(page)
 with open(out_file, "a") as f:
     for i in links:
         f.write(i)
-
-# req = requests.get(input("Enter Url: "))
-# soup = BeautifulSoup(req.text, "lxml")
-# pages = soup.find('a', attrs={'href': re.compile(".*download=.*govid.*")}).get('href')
-#
-# req = requests.get(pages).text
-# soup = BeautifulSoup(req,"lxml")
-# # Quality Change -->(5)<--   [6 for 720]  [5 for 480]  [4 for 360]
-# pages = soup.find('a',attrs={'onclick': re.compile(".*( 5.* ).*")}).get('href')
-# print(pages)
--- CimaclubSeries.py
+++ CimaclubSeries.py
@@ -1,25 +1,23 @@
 from bs4 import BeautifulSoup
-import requests
-import re
+from requests import get
+from re import compile
 from os import path
 
 '''
-This Script takes the first watch link and returns all season download links
+This Script takes the first watch link and returns a file containing all Eps download links
 '''
-
 
 dct = path.dirname(path.abspath(__file__))
 
-
-out_file = f"{dct}" + "\Links.txt"
+out_file = f"{dct}" + "\SLinks.txt"
 
 
 def next_epo(link):
     if link:
         page = link.get('href')
-        req = requests.get(page)
+        req = get(page)
         soup = BeautifulSoup(req.text, "lxml")
-        page = soup.find('a', attrs={'href': re.compile(".*download=.*govid.*")}).get('href')
+        page = soup.find('a', attrs={'href': compile(".*download=.*govid.*")}).get('href')
         mid_links.append(page)
         print('....')
         nxt = soup.find('a', attrs={'class': 'NextEpisode'})
@@ -28,11 +26,11 @@ def next_epo(link):
 
 def last_links(mid):
     x = 0
-    for i in mid:
-        req = requests.get(i).text
+    for li in mid:
+        req = get(li).text
         soup = BeautifulSoup(req, "lxml")
         # Quality Change -->(5)<--   [6 for 720]  [5 for 480]  [4 for 360]
-        page = soup.find('a', attrs={'onclick': re.compile(".*( 5.* ).*")}).get('href')
+        page = soup.find('a', attrs={'onclick': compile(".*( 5.* ).*")}).get('href')
         x += 1
         print(f'Ep {x}')
         ep.append(page)
@@ -40,14 +38,16 @@ def last_links(mid):
 
 ep = []
 mid_links = []
-first_link = input("Enter First Ep watch link: ")
-first_req = requests.get(first_link)
+first_link = input("Enter First Ep of a season watch link: ")
+first_req = get(first_link)
 first_soup = BeautifulSoup(first_req.text, "lxml")
-first_page = first_soup.find('a', attrs={'href': re.compile(".*download=.*govid.*")}).get('href')
+first_page = first_soup.find('a', attrs={'href': compile(".*download=.*govid.*")}).get('href')
 mid_links.append(first_page)
 
 first_nxt = first_soup.find('a', attrs={'class': 'NextEpisode'})
 next_epo(first_nxt)
 last_links(mid_links)
 
-print(ep)
+with open(out_file, "a") as out:
+    for i in ep:
+        out.write(i)
